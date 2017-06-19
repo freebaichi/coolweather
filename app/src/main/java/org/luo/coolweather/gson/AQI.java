ab6925b1545c74e8033214938c22dfc802fcda06
freebaichi <1170159388@qq.com>
2017-06-19 21:01:38 +0800

    基本完成开发

---
+++ app/src/main/java/org/luo/coolweather/gson/AQI.java
@@ -0,0 +1,15 @@
+package org.luo.coolweather.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
